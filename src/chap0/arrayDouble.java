86ecb0169ea6946c02c7f1916bd8931a04d8b38d
yjpotato <ponyojoa7467@gmail.com>
2023-04-03 22:08:21 +0900

    Initial commit

---
+++ src/chap0/arrayDouble.java
@@ -0,0 +1,10 @@
+package chap0;
+//정수 배열 numbers가 매개변수로 주어집니다. numbers의 각 원소에 두배한 원소를 가진 배열을 return하도록 solution 함수를 완성해주세요.
+public class arrayDouble {
+        public int solution(int num1, int num2) {
+            int answer = num1 % num2;
+            return answer;
+        }
+    }
+
+
